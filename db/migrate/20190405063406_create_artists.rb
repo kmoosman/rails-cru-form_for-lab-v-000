188c21202b78811b9cf88af4ed46711d8dd98154
Katie Moosman <moosman.katie@gmail.com>
2019-04-05 06:35:10 +0000

    Automatically backed up by Learn

--- db/migrate/20190405063406_create_artists.rb
+++ db/migrate/20190405063406_create_artists.rb
@@ -1,7 +1,8 @@
 class CreateArtists < ActiveRecord::Migration[5.0]
   def change
     create_table :artists do |t|
-
+      t.string :name, null: false
+      t.text :bio, null: false
       t.timestamps
     end
   end
